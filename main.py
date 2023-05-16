c0ef2ba2a0ffb4533f4699e06d25022069204fd4
eric-owen <ejo.owen@icloud.com>
2023-05-16 12:21:51 -0400

    adds program logic for checking reservation and booking

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
 import pandas
 
-df = pandas.read_csv("hotels.csv")
+df = pandas.read_csv("hotels.csv", dtype={"id": str})
 class Hotel:
-    def __init__(self, id):
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
+        self.name = df.loc[df["id"] == self.hotel_id, "name"].squeeze()
         pass
     def book(self):
-        pass
+        df.loc[df["id"] == self.hotel_id, "available"] = "no"
+        df.to_csv("hotels.csv", index=False)
     def available(self):
-        pass
+        availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
+        if availability == "yes":
+            return True
+        else:
+            return False
 
 class ReservationTicket:
     def __init__(self, cust_name, hotel_obj):
-        pass
+        self.cust_name = cust_name
+        self.hotel = hotel_obj
     def generate(self):
-        pass
+        content = f"""
+        Booking data:
+        Name: {self.cust_name}
+        hotel: {self.hotel.name}
+        """
+        return content
 
 print(df)
-id = input("enter the id of hotel: ")
-hotel = Hotel(id)
+hotel_ID = input("enter the id of hotel: ")
+hotel = Hotel(hotel_ID)
 if hotel.available():
     hotel.book()
-    name = input("senter your name: ")
-    reservation_ticket = ReservationTicket(name, hotel)
-    ;print(reservation_ticket.generate())
+    name = input("enter your name: ")
+    reservation_ticket = ReservationTicket(cust_name=name, hotel_obj=hotel)
+    print(reservation_ticket.generate())
 else:
     print("hotel is not available")
